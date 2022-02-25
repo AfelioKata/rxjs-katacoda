eea13a216c0666ece0b02cea3bba7a3b7775c6e5
Piero Sarpante <p.sarpante@afelio.be>
2022-02-25 09:29:18 +0100

    feat(services): create class

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PostCommentModel } from '@models/post-comment.model';
-import { PostModel } from '@models/post.model';
+import { PostModel, PostModelDTO } from '@models/post.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,14 +14,20 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   public getPosts(): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>(`${environment.path_api}posts`);
+    return this.http.get<PostModelDTO[]>(`${environment.path_api}posts`).pipe(
+      map((posts: PostModelDTO[]) => posts.map((e: PostModelDTO) => PostModel.fromDto(e)))
+    );
   }
 
   public getPostById(id: number): Observable<PostModel> {
-    return this.http.get<PostModel>(`${environment.path_api}posts/${id}`);
+    return this.http.get<PostModel>(`${environment.path_api}posts/${id}`).pipe(
+      map((post: PostModelDTO) => PostModel.fromDto(post))
+    )
   }
 
   public getPostsComments(idPost: number): Observable<PostCommentModel[]> {
-    return this.http.get<PostCommentModel[]>(`${environment.path_api}posts/${idPost}/comments`);
+    return this.http.get<PostCommentModel[]>(`${environment.path_api}posts/${idPost}/comments`).pipe(
+      map((comments: PostCommentModel[]) => comments.map((e: PostCommentModel) => PostCommentModel.fromDto(e)))
+    );
   }
 }
