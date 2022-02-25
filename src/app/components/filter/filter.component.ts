6ea88f0ead45426b7f7650939034e747103521a2
Piero Sarpante <p.sarpante@afelio.be>
2022-02-25 11:59:32 +0100

    feat(filter): add solution

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-filter',
@@ -10,7 +11,7 @@ export class FilterComponent implements OnInit {
 
   public ngOnInit(): void {
     from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).pipe(
-
+      filter(num => !(num % 2))
     ).subscribe((e: number) => console.log(e));
   }
 }
